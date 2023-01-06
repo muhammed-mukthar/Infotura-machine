8a71da6c570d336a132a122d6f5463cf819da680
Muhammed Mukthar <muktharmanaf28@gmail.com>
2023-01-06 21:24:18 +0530

    user log

--- controller/FreelancerController.js
+++ controller/FreelancerController.js
@@ -1,24 +1,24 @@
 const FreelancerModel = require("../models/FreelancerModel");
-exports.LoginHandler= async (req, res) => {
-    try {
-      console.log(req.body);
-      const user = await FreelancerModel.findOne({ email: req.body.email });
-      if (!user) {
-       return res.json({ err: "user not found" });
-      }
-      const isValid = await user.comparePassword(req.body.password);
-      console.log(isValid);
-      if (!isValid) {
-          return  res.json({ err: "invalid password" });
-      }
-      const userDetails = {
-        _id: user._id,
-        email: user.email,
-      };
-    
-      console.log(accessToken);
-      res.json({ data: userDetails });
-    } catch (error) {
-      res.status(500).json({ err: error });
+exports.LoginHandler = async (req, res) => {
+  try {
+    console.log(req.body);
+    const user = await FreelancerModel.findOne({ email: req.body.email });
+    if (!user) {
+      return res.json({ err: "user not found" });
     }
+    const isValid = await user.comparePassword(req.body.password);
+    console.log(isValid);
+    if (!isValid) {
+      return res.json({ err: "invalid password" });
+    }
+    const userDetails = {
+      _id: user._id,
+      email: user.email,
+    };
+
+    console.log(accessToken);
+    res.json({ data: userDetails });
+  } catch (error) {
+    res.status(500).json({ err: error });
   }
+};
