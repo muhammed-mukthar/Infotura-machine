ff83c05e2355e9b64ec0a9a2f279cb2264934e4e
Muhammed Mukthar <muktharmanaf28@gmail.com>
2023-01-06 20:51:56 +0530

    server set up

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv").config();
+const mongoose = require("mongoose");
+
+const cors=require('cors')
+const morgan = require("morgan");
+app.use(express.json());
+app.use(morgan("common"));
+app.use(cors("*"));
+
+const port = process.env.PORT || 5000;
+mongoose.set("strictQuery", false);
+
+mongoose.connect(process.env.DB_CONNECT, {
+  useNewUrlParser: true,
+
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("db Connected successfully");
+});
+
+
+app.listen(process.env.PORT, () => {
+  console.log(`port running on ${port}`);
+});
